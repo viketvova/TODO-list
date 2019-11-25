fc3e9fc7830b9d6bbfbdefd9e25c573e676767e6
Vitaly <viketvova@gmail.com>
2019-11-25 20:33:02 +0200

    Create TodoListHeader, TodoListFooter, TodoListTasks

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import React from 'react';
 import './App.css';
+import TodoListHeader from './TodoListHeader';
+import TodoListFooter from './TodoListFooter ';
+import TodoListTasks from './TodoListTasks';
 
 class App extends React.Component {
     render = () => {
         return (
             <div className="App">
                 <div className="todoList">
-                    <div className="todoList-header">
-                        <h3 className="todoList-header__title">What to Learn</h3>
-                        <div className="todoList-newTaskForm">
-                            <input type="text" placeholder="New task name"/>
-                            <button>Add</button>
-                        </div>
-                    </div>
-                    <div className="todoList-tasks">
-                        <div className="todoList-task">
-                            <input type="checkbox" checked={true}/>
-                            <span>CSS</span>
-                        </div>
-                        <div className="todoList-task">
-                            <input type="checkbox" checked={false}/>
-                            <span>JS</span>
-                        </div>
-                        <div className="todoList-task">
-                            <input type="checkbox" checked={false}/>
-                            <span>ReactJS</span>
-                        </div>
-                        <div className="todoList-task">
-                            <input type="checkbox" checked={true}/>
-                            <span>Patterns</span>
-                        </div>
-                    </div>
-                    <div className="todoList-footer">
-                        <button>All</button>
-                        <button>Completed</button>
-                        <button>Active</button>
-                    </div>
+                    <TodoListHeader />
+                    <TodoListTasks />
+                    <TodoListFooter />
                 </div>
             </div>
         );
