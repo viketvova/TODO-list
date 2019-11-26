fd7586b51cf1ec170168faa650d79f24a0caa932
Vitaly <viketvova@gmail.com>
2019-11-26 21:59:47 +0200

    Add new tasks

--- src/App.js
+++ src/App.js
@@ -2,27 +2,52 @@ import React from 'react';
 import './App.css';
 import TodoListFooter from './TodoListFooter';
 import TodoListTasks from './TodoListTasks';
-import TodoListHeader from './TodoListHeader';
+//import TodoListHeader from './TodoListHeader';
 
 class App extends React.Component {
-    
-    tasks = [
-        { title: "JS", isDone: true, priority: "low"},
-        { title: "CSS", isDone: true, priority: "high"},
-        { title: "HTML", isDone: true, priority: "medium"},
-        { title: "React", isDone: false, priority: "high"}
-    ];
-    
-    filterValue = "Active";
+    constructor(props) {
+        super(props);
+        this.newTaskTitleRef = React.createRef();
+        }
+    state = {
+        tasks: [
+            { title: "JS", isDone: true, priority: "low" },
+            { title: "CSS", isDone: true, priority: "high" },
+            { title: "HTML", isDone: true, priority: "medium" },
+            { title: "React", isDone: false, priority: "high" }
+        ],
+        filterValue: "Active"
+    };
+    onAddTaskClick = () => {
+        let newInput = this.newTaskTitleRef.current.value;
+        let newTask = {
+            title: newInput,
+            isDOne: false,
+            priority: 'low'
+         };
+         let newTasks = [...this.state.tasks, newTask];
+         this.setState ({
+             tasks: newTasks
+         });
+         this.newTaskTitleRef.current.value = "";
+         
+     }
 
     render = () => {
-        
+console.log(this.onAddTaskClick)
         return (
             <div className="App">
                 <div className="todoList">
-                    <TodoListHeader />
-                    <TodoListTasks tasks={this.tasks}/>
-                    <TodoListFooter filterValue={this.filterValue}/>
+                    {/* <TodoListHeader button={this.onAddTaskClick} /> */}
+                    <div className="todoList-header">
+                <h3 className="todoList-header__title">What to Learn</h3>
+                <div className="todoList-newTaskForm">
+                    <input ref={this.newTaskTitleRef} type="text" placeholder="New task name" />
+                    <button onClick={this.onAddTaskClick}>Add</button>
+                </div>
+            </div>
+                    <TodoListTasks tasks={this.state.tasks} />
+                    <TodoListFooter filterValue={this.state.filterValue} />
                 </div>
             </div>
         );
--- src/TodoListHeader.js
+++ src/TodoListHeader.js
@@ -4,14 +4,15 @@ import './App.css';
 class TodoListHeader extends React.Component {
 
     render = () => {
+        console.log(this.props)
         return (
-            <div className="todoList-header">
-                <h3 className="todoList-header__title">What to Learn</h3>
-                <div className="todoList-newTaskForm">
-                    <input type="text" placeholder="New task name" />
-                    <button>Add</button>
-                </div>
-            </div>
+            // <div className="todoList-header">
+            //     <h3 className="todoList-header__title">What to Learn</h3>
+            //     <div className="todoList-newTaskForm">
+            //         <input type="text" placeholder="New task name" />
+            //         <button onClick={this.props.onAddTaskClick}>Add</button>
+            //     </div>
+            // </div>
         )
 
     }
