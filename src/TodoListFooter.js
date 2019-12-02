b1d3d74937fbfd4902388c1b725e49155dd66725
Vitaly <viketvova@gmail.com>
2019-12-02 15:54:44 +0200

    Add <span> hide/show

--- src/TodoListFooter.js
+++ src/TodoListFooter.js
@@ -3,20 +3,40 @@ import './App.css';
 //import s from './TodoListFooter.module.css';
 
 class TodoListFooter extends React.Component {
-    render = () => {
+  state = {
+    isHidden: false
+  }
+    changeStatus = () => {
+      if(this.state.isHidden === false) {
+        this.setState({
+        isHidden:true
+        })
+      }else{
+        this.setState({
+        isHidden:false
+        })
+      }
+    }
+    render = (props) => {
         
         let classForAll = this.props.filterValue === "All" ? "filter-active" : "";
         let classForCompleted = this.props.filterValue === "Completed" ? "filter-active" : "";
         let classForActive = this.props.filterValue === "Active" ? "filter-active" : "";
-
+console.log(this.setState)
         return (
+
             <div className="todoList-footer">
+              {!this.state.isHidden && <div>
                 <button onClick={ () => {this.props.changeFilter("All")}} 
                 className={classForAll}>All</button>
                 <button onClick={ () => {this.props.changeFilter("Completed")}} 
                 className={classForCompleted}>Completed</button>
                 <button onClick={ () => {this.props.changeFilter("Active")}} 
                 className={classForActive}>Active</button>
+              </div>
+              }
+                {!this.state.isHidden && <span onClick={this.changeStatus}>hide</span>}
+                {this.state.isHidden && <span onClick={this.changeStatus}>show</span>}
             </div>
         );
     }
